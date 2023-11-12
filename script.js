4e93e02e05765e4944924685f1602868134ac02e
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-12 14:41:06 +0530

    written js code to add multiple tasks

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addTask=document.getElementById("tasks");
 const inputTask=document.getElementById("add");
 
 
+
 function display()
 {
     if(inputTask.value==="")
@@ -11,6 +12,13 @@ function display()
     }
     else
     {
-        addTask.innerHTML=inputTask.value
+        const newTask=document.createElement("div");
+        const text=`<div id="ft"><img src="radioUnchecked.png" alt="" width="20px" style="margin-right:20px" onclick="flip();" id="radiauc">${inputTask.value}<img src="cutLogo.png" alt="" width="20px" style="margin-left:20px"  onclick="remove();" ></div>`;
+        newTask.innerHTML=text;
+        tasks.appendChild(newTask);
+        inputTask.value="";
     }
 }
+
+
+
