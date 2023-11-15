f140961e4a64fc9cd5f446b90f8b76b979e59865
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-15 20:20:17 +0530

    tasks added successfully

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+const addTask = document.getElementById("tasks");
+const inputTask = document.getElementById("add");
 
-const addTask=document.getElementById("tasks");
-const inputTask=document.getElementById("add");
-
-
-
-function display()
-{
-    if(inputTask.value==="")
-    {
-        alert("Please enter a task first");
-    }
-    else
-    {
-        const newTask=document.createElement("div");
-        const text=`<div id="ft"><img src="radioUnchecked.png" alt="" width="20px" style="margin-right:20px" onclick="flip();" id="radiauc">${inputTask.value}<img src="cutLogo.png" alt="" width="20px" style="margin-left:20px"  onclick="remove();" ></div>`;
-        newTask.innerHTML=text;
-        tasks.appendChild(newTask);
-        inputTask.value="";
-    }
+function display() {
+  if (inputTask.value === "") 
+  {
+    alert("Please enter a task first");
+  } 
+  else 
+  {
+    const task = document.createElement("div");
+    const text=`<div class="taskStyle" onclick="flip();">
+    ${inputTask.value}
+     </div>
+     <img src="cutLogo.png" alt=""class="crossLogo"  >`;
+    task.innerHTML = text;
+    task.style.fontSize="25px";
+    task.firstElementChild.id="cont";
+    addTask.appendChild(task);
+    inputTask.value = "";
+  }
 }
 
-
-
+function flip()
+{
+  let div2=document.querySelector("#cont");
+  div2.classList.toggle("flipImage");
+}
